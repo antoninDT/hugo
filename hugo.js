1ab563f64a807307a066c6ecaafc4dba7d540a92
Antonin Dujardin-Terry <mrmonkeyfisher@gmail.com>
2018-02-17 09:06:36 +0100

    Refactored state and actions into new game object

--- hugo.js
+++ hugo.js
@@ -10,192 +10,183 @@ const lineReader = readline.createInterface({
 });
 
 const items = Object.values(itemsLookup);
+const getItemById = (itemId) => items.find((item) => item.id === itemId);
 const rooms = Object.values(roomsLookup);
 const commands = Object.values(commandLookup);
 const defaultRoomId = roomsLookup.hall.id;
-
-const itemIdToWin = items[Math.floor(Math.random() * items.length)].id;
 
 const player = {
     currentRoomId: defaultRoomId,
     inventory: [],
 };
 
-const goodbye = () => {
-    console.log(`
-    
-    
-    @@@@@@@@@@@@@@@@@@@@@
-        DOE DOEI!!!!!!
-    @@@@@@@@@@@@@@@@@@@@@
-    
-    
-    `);
-    process.exit();
-};
-
-const didPlayerWin = () => {
-    return (player.inventory.includes(itemIdToWin));
-};
-
-const showWinScreen = () => {
-    console.log(`
-
-        Congratulations! You have found the hidden Item!
-
-    `);
-    goodbye();
-};
-
-const moveItem = (itemIdToMove, source, destination) => {
-    const newSourceItems = source.inventory.filter((itemId) => itemId !== itemIdToMove);
-    source.inventory = newSourceItems;
-    destination.inventory.push(itemIdToMove);
-};
-
-const moveItemFromCurrentRoomToPlayer = (itemName) => {
-    if (!itemName) {
-        console.log(`
+const game = {
+    state: {
+      player,
+      rooms,
+      itemIdToWin: items[Math.floor(Math.random() * items.length)].id,
+    },
+    actions: {
+        moveItem(itemIdToMove, source, destination) {
+            const newSourceItems = source.inventory.filter((itemId) => itemId !== itemIdToMove);
+            source.inventory = newSourceItems;
+            destination.inventory.push(itemIdToMove);
+        },
+        movePlayerToRoom(roomId = defaultRoomId) {
+            game.state.player.currentRoomId =  roomId;
+            game.showCurrentRoom();
+        },
+        moveItemFromCurrentRoomToPlayer(itemName) {
+            if (!itemName) {
+                console.log(`
         
                                     😂😂😂
-        You forgot to put the name of the item to pick up stupid... try again! 
+                    You forgot to put the name of the item to pick up hoor... try again! 
                                     😂😂😂
         
-        `);
-        return;
-    }
-    const room = rooms.find((room) => room.id === player.currentRoomId);
-    if (!room.inventory || !room.inventory.length) {
-        console.log(`
+                `);
+                return;
+            }
+            const room = game.state.rooms.find((room) => room.id === game.state.player.currentRoomId);
+            if (!room.inventory || !room.inventory.length) {
+                console.log(`
         
-        🕸️🕸️🕸️🕸️🕸️🕸️🕸️🕸️️
-        room is empty... 
-        🕸️🕸️🕸️🕸️🕸️🕸️🕸️🕸️
+                    🕸️🕸️🕸️🕸️🕸️🕸️🕸️🕸️️
+                    room is empty... 
+                    🕸️🕸️🕸️🕸️🕸️🕸️🕸️🕸️
         
-        `);
-        return;
-    }
-    const item = items.find((item) => item.name.toLowerCase() === itemName.toLowerCase());
-    if (!item || !room.inventory.includes(item.id)) {
-        console.log(`
+                `);
+                return;
+            }
+            const item = items.find((item) => item.name.toLowerCase() === itemName.toLowerCase());
+            if (!item || !room.inventory.includes(item.id)) {
+                console.log(`
         
-            The current room does not have "${itemName}"
+                        The current room does not have "${itemName}"
         
-        `);
-        return;
-    }
-    moveItem(item.id, room, player);
-    console.log(`
+                `);
+                return;
+            }
+            this.moveItem(item.id, room, player);
+            console.log(`
     
-        You have put "${item.name}" into your inventory
+                    You have put "${item.name}" into your inventory
     
-    `);
-    if (didPlayerWin()) {
-        showWinScreen();
-    }
-};
+            `);
+            if (game.didPlayerWin()) {
+                game.showWinScreen();
+            }
+        },
+    },
+    didPlayerWin() {
+        return (this.state.player.inventory.includes(this.state.itemIdToWin));
+    },
+    goodbye() {
+        console.log(`
+    
+    
+            @@@@@@@@@@@@@@@@@@@@@
+                DOE DOEI!!!!!!
+            @@@@@@@@@@@@@@@@@@@@@
+    
+    
+        `);
+        process.exit();
+    },
+    showWinScreen() {
+        console.log(`
 
-const showRooms = () => {
-    console.log('Here are the rooms:');
-    const getRoomName = (room) => room.name;
-    const showRoomName = (roomName) => console.log(`    * ${roomName}`);
-    rooms
-        .map(getRoomName)
-        .forEach(showRoomName);
-};
+            Congratulations! You have found the hidden Item!
 
-const getCurrentRoom = () => {
-    const result = rooms.find((room) => room.id === player.currentRoomId);
-    return result;
-};
-
-const getItemById = (itemId) => items.find((item) => item.id === itemId);
-
-const showItem = (item) => {
-    console.log(`
+        `);
+        this.goodbye();
+    },
+    showRooms() {
+        console.log('Here are the rooms:');
+        const getRoomName = (room) => room.name;
+        const showRoomName = (roomName) => console.log(`    * ${roomName}`);
+        this.state.rooms
+            .map(getRoomName)
+            .forEach(showRoomName);
+    },
+    getCurrentRoom() {
+        const result = this.state.rooms.find((room) => room.id === this.state.player.currentRoomId);
+        return result;
+    },
+    showItem(item) {
+        console.log(`
                 * (${item.name}) ${item.description || ''}
         `);
-};
-
-const showCurrentRoomContents = () => {
-    const currentRoom = getCurrentRoom();
-    if (!(currentRoom.inventory && currentRoom.inventory.length)) {
-        console.log(`
+    },
+    showCurrentRoomContents() {
+        const currentRoom = this.getCurrentRoom();
+        if (!(currentRoom.inventory && currentRoom.inventory.length)) {
+            console.log(`
     
             You look around and notice that the room is empty...
             💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩 
     
         `);
-        return;
-    }
-    console.log(`
-    
-        You look around and notice the following items: 
-    
-    `);
-    currentRoom.inventory
-        .map(getItemById)
-        .forEach(showItem);
-};
-
-const showInventory = () => {
-    if (!player.inventory.length) {
+            return;
+        }
         console.log(`
     
-            There is nothing in your Inventory... 
+            You look around and notice the following items: 
+    
+        `);
+        currentRoom.inventory
+            .map(getItemById)
+            .forEach(this.showItem);
+    },
+    showInventory() {
+        if (!this.state.player.inventory.length) {
+            console.log(`
+    
+                There is nothing in your Inventory... 
             
                           😐😐😐
     
+            `);
+            return;
+        }
+        console.log(`
+    
+            You have the following items: 
+    
         `);
-        return;
-    }
-    console.log(`
-    
-        You have the following items: 
-    
-    `);
-    player.inventory
-        .map(getItemById)
-        .forEach(showItem);
-};
-
-const clearScreen = () => {
-    const numberOfLines = 100;
-    for (let i = 0; i < numberOfLines; i++) {
+        this.state.player.inventory
+            .map(getItemById)
+            .forEach(this.showItem);
+    },
+    // TODO: Add a "show status" function and then use it.
+    clearScreen() {
+        const numberOfLines = 100;
+        for (let i = 0; i < numberOfLines; i++) {
+            console.log();
+        }
+    },
+    welcomeMessage() {
         console.log();
-    }
-};
-
-// TODO: Add a "show status" function and then use it.
-
-const welcomeMessage = () => {
-    console.log();
-    console.log(' ######### Welkom bij Hugo Hulp ######### ');
-    console.log();
-    console.log('Can you find the hidden item??');
-    showRooms();
-    showCurrentRoom();
-};
-
-const showCurrentRoom = () => {
-    const currentRoomName = getCurrentRoom().name;
-    console.log(`
+        console.log(' ######### Welkom bij Hugo Hulp ######### ');
+        console.log();
+        console.log('Can you find the hidden item??');
+        this.showRooms();
+        this.showCurrentRoom();
+    },
+    showCurrentRoom() {
+        const currentRoomName = this.getCurrentRoom().name;
+        console.log(`
      
-     You are in the ${currentRoomName}.
+            You are in the ${currentRoomName}.
      
-     `);
-};
-
-const showHelp = () => {
-    console.log(' ===== List of commands =====');
-    commands.forEach((command) => {
-        console.log(`  * ${command.command}: ${command.description}`);
-    });
-};
-
-const movePlayerToRoom = (roomId = defaultRoomId) => {
-    player.currentRoomId = roomId;
-    showCurrentRoom();
+        `);
+    },
+    showHelp() {
+        console.log(' ===== List of commands =====');
+        commands.forEach((command) => {
+            console.log(`  * ${command.command}: ${command.description}`);
+        });
+    },
 };
 
 const processInput = (prompt, handler) => {
@@ -209,7 +200,7 @@ const promptForUserCommand = () => {
             .trim();
         switch (true) {
             case (sanitizedInput.startsWith(commandLookup.exit.command)):
-                goodbye();
+                game.goodbye();
                 return;
             case (sanitizedInput.startsWith(commandLookup.goTo.command)):
                 const roomParts = sanitizedInput
@@ -228,28 +219,28 @@ const promptForUserCommand = () => {
                     break;
                 };
                 const roomId = foundRoom.id;
-                movePlayerToRoom(roomId);
+                game.actions.movePlayerToRoom(roomId);
                 break;
             case (sanitizedInput.startsWith(commandLookup.help.command)):
-                showHelp();
+                game.showHelp();
                 break;
             case (sanitizedInput.startsWith(commandLookup.showRooms.command)):
-                showRooms();
+                game.showRooms();
                 break;
             case (sanitizedInput.startsWith(commandLookup.whereAmI.command)):
-                showCurrentRoom();
+                game.showCurrentRoom();
                 break;
             case (!sanitizedInput): {
                 break;
             }
             case (sanitizedInput.startsWith(commandLookup.clear.command)):
-                clearScreen();
+                game.clearScreen();
                 break;
             case (sanitizedInput.startsWith(commandLookup.lookAround.command)):
-                showCurrentRoomContents();
+                game.showCurrentRoomContents();
                 break;
             case (sanitizedInput.startsWith(commandLookup.showInventory.command)):
-                showInventory();
+                game.showInventory();
                 break;
             case (sanitizedInput.startsWith(commandLookup.transferItemToPlayerInventory.command)):
                 const itemParts = sanitizedInput
@@ -258,7 +249,7 @@ const promptForUserCommand = () => {
                     .toLowerCase()
                     .replace('the', '')
                     .trim();
-                moveItemFromCurrentRoomToPlayer(itemName);
+                game.actions.moveItemFromCurrentRoomToPlayer(itemName);
                 break;
             default:
                 console.log(`
@@ -272,5 +263,5 @@ const promptForUserCommand = () => {
     processInput('Please enter a command:', handleCommand);
 };
 
-welcomeMessage();
+game.welcomeMessage();
 promptForUserCommand();
