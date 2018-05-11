b15e47af6e201d2eb9cb3e617c9293b6b4f66fb8
Antonin Dujardin-Terry <mrmonkeyfisher@gmail.com>
2018-05-11 10:28:10 +0200

    Created a new file called voices.utility

--- game.js
+++ game.js
@@ -327,6 +327,4 @@ const api = {
   getNewGame
 };
 
-// TODO: Create a new speechQueue which allows you to add requests to say something. The queue will have a timer to check every half-second for items in the queue to speak, and then it will say each of those items in order (waiting for one to finish before the next one, until there are none left, and then start the timer). Once finished: replace all usages of "say" with calls to the queue.
-
 module.exports = api;
