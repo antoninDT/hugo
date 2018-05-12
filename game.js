aea4cc8b59b9b9bd8e3d9bbbf65f7c835974dd2f
Antonin Dujardin-Terry <mrmonkeyfisher@gmail.com>
2018-05-12 10:28:14 +0200

    Refactored sampleVoices into voice.utility.js

--- game.js
+++ game.js
@@ -6,7 +6,7 @@ const chalkAnimation = require('chalk-animation');
 const { basicBoxOptions, basicCFontOptions, getTextColorBasedOnCurrentTime, consoleOutPut, clearScreenWrapper, } = require('./console.utility');
 const { defaultRoomId, getRoomById, roomsLookup, rooms, showCurrentRoomWrapper, showRoomsWrapper, showCurrentRoomContentsWrapper, getCurrentRoomWrapper, randomlyDistributeItemsToRoomsWrapper, randomlyDistributeEnemiesToRoomsWrapper, randomlyDistributeHealersToRoomsWrapper, showEnemyAttackMessageWrapper } = require('./room.utility');
 const { getRandomArrayItem } = require('./general.utility');
-const { addSentenceToSpeechQueue } = require('./voices.utility');
+const { addSentenceToSpeechQueue, sampleVoicesWrapper } = require('./voices.utility');
 const { getItemByIdWrapper, getEnemyByIdWrapper, getHealerByIdWrapper, showEnemyOrHealerWrapper, getCurrentRoomClueWrapper, getCurrentItemClueWrapper, giveItemClueWrapper, getRandomItemIdToWinWrapper, craftItemWrapper, spawnItemWrapper } = require('./item.utility');
 const { dealDamageIfNeededWrapper, healPlayerIfNeededWrapper, moveItemFromCurrentRoomToPlayerWrapper, moveItemWrapper, movePlayerToRoomWrapper, movePlayerToRandomRoomWrapper, hurtPlayerWrapper, healPlayerWrapper, moveItemFromPlayerToCurrentRoomWrapper, showPlayerStatusWrapper, showInventoryWrapper } = require('./player.utility');
 
@@ -92,112 +92,6 @@ const game = {
   },
   getTextColorBasedOnCurrentTime,
   consoleOutPut,
-  sampleVoices() { // TODO REFACTOR: Move this into a new voice.utility.js file
-    const voices = [
-      'Agnes',
-      'Albert',
-      'Alex',
-      'Alice',
-      'Alva',
-      'Amelie',
-      'Anna',
-      'Bad News',
-      'Bahh',
-      'Bells',
-      'Boing',
-      'Bruce',
-      'Bubbles',
-      'Carmit',
-      'Cellos',
-      'Damayanti',
-      'Daniel',
-      'Deranged',
-      'Diego',
-      'Ellen',
-      'Fiona',
-      'Fred',
-      'Good news',
-      'Hysterical',
-      'Ioana',
-      'Joana',
-      'Jorge',
-      'Juan',
-      'Kanya',
-      'Karen',
-      'Kate',
-      'Kathy',
-      'Kyoko',
-      'Laura',
-      'Lee',
-      'Lekha',
-      'Luca',
-      'Luciana',
-      'Maged',
-      'Mariska',
-      'Mei-Jia',
-      'Melina',
-      'Milena',
-      'Moira',
-      'Monica',
-      'Nora',
-      'Oliver',
-      'Paulina',
-      'Pipe Organ',
-      'Princess',
-      'Ralph',
-      'Samantha',
-      'Sara',
-      'Satu',
-      'Serena',
-      'Sin-ji',
-      'Tessa',
-      'Thomas',
-      'Ting-Ting',
-      'Trinoids',
-      'Veena',
-      'Vicki',
-      'Victoria',
-      'Whisper',
-      'Xander',
-      'Yelda',
-      'Yuna',
-      'Yuri',
-      'Zarvox',
-      'Zosia',
-      'Zuzana'
-    ];
-
-    const sampleVoice = (index = 0) => {
-      const voice = voices[index];
-      if (!voice) {
-        return;
-      }
-      game.consoleOutPut({ text: `DEBUG: sampleVoice() voice: ${voice}` }); //TODO: Kill this line
-      const speakSampleSentence2 = (voice) => {
-        const sampleSentence2 = 'Airam and her boyfriends, including Isreal and Connor';
-        say.speak(sampleSentence2, voice, 1, () => sampleVoice(index + 1));
-      };
-      const speakSampleSentence1 = (voice) => {
-        const sampleSentence = `This is a sample sentence for the game. La la la la! Ma ma ma ma! Ra ra Ba ba pa pa, fa fa fa fa fa
-
-                These three skis through the trees to my knees but without all those tricky tricky stinky slinky bees. Who? Why? What! OK! whatever
-
-                `;
-        say.speak(sampleSentence, voice, 1, () => sampleVoice(index + 1));
-      };
-      const speakSampleSentenceForVoice = () => {
-        speakSampleSentence2(voice);
-      };
-      const speakAnnouncement = () => {
-        say.speak(`Sample voice for: ${voice}`, 'Samantha', 1, speakSampleSentenceForVoice);
-      };
-      speakAnnouncement();
-    };
-    const speakGreeting = () => {
-      say.speak(`OK, now I'm going to list all of the possible voices for you: `, 'Samantha', 1, () => sampleVoice());
-    };
-    speakGreeting();
-  },
   welcomeMessage() { // TODO: Add promises so that voice can work
     const welcomeMessageOptions = {
       ...basicCFontOptions,
@@ -276,6 +170,7 @@ const game = {
 };
 
 const wireUpImportedGameFunctions = () => {
+  game.sampleVoices = sampleVoicesWrapper(game);
   game.showInventory = showInventoryWrapper(game);
   game.showPlayerStatus = showPlayerStatusWrapper(game);
   game.moveItemFromPlayerToCurrentRoom = moveItemFromPlayerToCurrentRoomWrapper(game); //TODO: Does this need to be in action?
--- voices.utility.js
+++ voices.utility.js
@@ -22,7 +22,115 @@ const addSentenceToSpeechQueue = (speechOptions) => {
   queue.push(speechOptions);
 };
 
+const sampleVoicesWrapper = (game) => () => { // TODO REFACTOR: Move this into a new voice.utility.js file
+  const voices = [
+    'Agnes',
+    'Albert',
+    'Alex',
+    'Alice',
+    'Alva',
+    'Amelie',
+    'Anna',
+    'Bad News',
+    'Bahh',
+    'Bells',
+    'Boing',
+    'Bruce',
+    'Bubbles',
+    'Carmit',
+    'Cellos',
+    'Damayanti',
+    'Daniel',
+    'Deranged',
+    'Diego',
+    'Ellen',
+    'Fiona',
+    'Fred',
+    'Good news',
+    'Hysterical',
+    'Ioana',
+    'Joana',
+    'Jorge',
+    'Juan',
+    'Kanya',
+    'Karen',
+    'Kate',
+    'Kathy',
+    'Kyoko',
+    'Laura',
+    'Lee',
+    'Lekha',
+    'Luca',
+    'Luciana',
+    'Maged',
+    'Mariska',
+    'Mei-Jia',
+    'Melina',
+    'Milena',
+    'Moira',
+    'Monica',
+    'Nora',
+    'Oliver',
+    'Paulina',
+    'Pipe Organ',
+    'Princess',
+    'Ralph',
+    'Samantha',
+    'Sara',
+    'Satu',
+    'Serena',
+    'Sin-ji',
+    'Tessa',
+    'Thomas',
+    'Ting-Ting',
+    'Trinoids',
+    'Veena',
+    'Vicki',
+    'Victoria',
+    'Whisper',
+    'Xander',
+    'Yelda',
+    'Yuna',
+    'Yuri',
+    'Zarvox',
+    'Zosia',
+    'Zuzana'
+  ];
+
+  const sampleVoice = (index = 0) => {
+    const voice = voices[index];
+    if (!voice) {
+      return;
+    }
+    game.consoleOutPut({ text: `DEBUG: sampleVoice() voice: ${voice}` }); //TODO: Kill this line
+    const speakSampleSentence2 = (voice) => {
+      const sampleSentence2 = 'blah blah blah';
+      say.speak(sampleSentence2, voice, 1, () => sampleVoice(index + 1));
+    };
+    const speakSampleSentence1 = (voice) => {
+      const sampleSentence = `This is a sample sentence for the game. La la la la! Ma ma ma ma! Ra ra Ba ba pa pa, fa fa fa fa fa
+
+              These three skis through the trees to my knees but without all those tricky tricky stinky slinky bees. Who? Why? What! OK! whatever
+
+              `;
+      say.speak(sampleSentence, voice, 1, () => sampleVoice(index + 1));
+    };
+    const speakSampleSentenceForVoice = () => {
+      speakSampleSentence1(voice);
+    };
+    const speakAnnouncement = () => {
+      say.speak(`Sample voice for: ${voice}`, 'Samantha', 1, speakSampleSentenceForVoice);
+    };
+    speakAnnouncement();
+  };
+  const speakGreeting = () => {
+    say.speak(`OK, now I'm going to list all of the possible voices for you: `, 'Samantha', 1, () => sampleVoice());
+  };
+  speakGreeting();
+};
+
 const api = {
+  sampleVoicesWrapper,
   addSentenceToSpeechQueue,
 };
 module.exports = api;
